2d765f0215c28534aa9bcb0e0440216697a23e45
Andres Jaan Tack <ajtack@gmail.com>
2010-04-20 01:50:50 -0500

    Removed an unnecessary bit of Javascript.

--- public/javascripts/views/about/index.js
+++ public/javascripts/views/about/index.js
@@ -3,6 +3,5 @@ $(document).ready(function() {
 	$('#AboutSections a').attr('href', function(_, previous) { return previous + '.js'});
 	
 	// Set up a tabbed selector.
-	$('#AboutSections').tabs().addClass('ui-tabs-vertical ui-helper-clearfix');
-	$('#AboutSections li').removeClass('ui-corner-top').addClass('ui-corner-left');
+	$('#AboutSections').tabs();
 });
